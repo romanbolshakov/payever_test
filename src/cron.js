929925d60705c778eab6e50cd6baf0b077b8514e
Roman Bolshakov <bolero2001@yandex.ru>
2019-07-17 15:18:09 +0200

    cron task fix issue

--- src/cron.js
+++ src/cron.js
@@ -1,16 +1,15 @@
 const cron = require('node-cron');
 const userService = require('./services/user.service');
 
-let page = 5;
+let page = 0;
 const fileName = require('path').resolve(__dirname, '../user_list.json');
 
 const task = cron.schedule('0-59 * * * *', async () => {
   console.log('Executed...');
   page++;
+  if (page === 60) page = 1;
   userService.getAndSaveUserListPage(page, fileName);
 }, {scheduled: false});
 
-//task.start();
-page++;
-userService.getAndSaveUserListPage(page, fileName);
+task.start();
 
